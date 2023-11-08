0d18a5e57ac072ecdcd5775c2ab2527c2e2a3d84
RFozzy688 <pokerfozzy@gmail.com>
2023-11-08 19:17:42 +0200

    Task для факториала

--- Parallel_hw/Program.cs
+++ Parallel_hw/Program.cs
@@ -10,6 +10,21 @@ namespace Parallel_hw
     {
         static void Main(string[] args)
         {
+            Program pr = new Program();
+            int num = 25;
+
+            Task<decimal> task = new Task<decimal>(() => pr.Factorial(num));
+            task.Start();
+            Console.WriteLine("Факториал {0}! = {1}", num, task.Result.ToString("#,#"));
+        }
+        decimal Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+        void NumberOfDigits(decimal n)
+        {
+            
         }
     }
 }
