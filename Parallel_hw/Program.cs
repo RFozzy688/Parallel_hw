da108a8e8c6703f1991560af9efb741c80fe7708
RFozzy688 <pokerfozzy@gmail.com>
2023-11-08 19:54:41 +0200

    параллельное выполнение методов

--- Parallel_hw/Program.cs
+++ Parallel_hw/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Parallel_hw
@@ -11,11 +12,17 @@ namespace Parallel_hw
         static void Main(string[] args)
         {
             Program pr = new Program();
-            int num = 25;
+            int num = 27;
 
             Task<decimal> task = new Task<decimal>(() => pr.Factorial(num));
             task.Start();
-            Console.WriteLine("Факториал {0}! = {1}", num, task.Result.ToString("#,#"));
+            decimal fact = task.Result;
+            Console.WriteLine("Факториал {0}! = {1}", num, fact.ToString("#,#"));
+
+            Parallel.Invoke(
+                () => pr.NumberOfDigits(fact), 
+                () => pr.SumOfDigits(fact)
+                );
         }
         decimal Factorial(int n)
         {
@@ -24,7 +31,26 @@ namespace Parallel_hw
         }
         void NumberOfDigits(decimal n)
         {
-            
+            int count = 0;
+
+            while (n > 1)
+            {
+                n /= 10;
+                count++;
+            }
+            Console.WriteLine("Количество цифр: {0}", count);
+        }
+        void SumOfDigits(decimal n)
+        {
+            int sum = 0;
+
+            while (n > 1)
+            {
+                int num = (int)(n % 10);
+                n /= 10;
+                sum += num;
+            }
+            Console.WriteLine("Сумма цифр: {0}", sum);
         }
     }
 }
