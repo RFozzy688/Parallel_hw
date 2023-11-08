245809e28aade35800966e4fbf121b1416c6659b
RFozzy688 <pokerfozzy@gmail.com>
2023-11-08 20:27:19 +0200

    заполнение массива

---
+++ Parallel_ForEach/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            List<int> list = new List<int>();
+
+            pr.FillArray(list);
+        }
+        void FillArray(List<int> arr)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                arr.Add(rnd.Next(1, 20));
+            }
+        }
+    }
+}
